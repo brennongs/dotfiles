c1d09a7da567a17e4d5d446ee89306bbd28c5598
Brennon Schow <bschow@crossroadstech.net>
2018-11-13 15:19:45 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -9,14 +9,14 @@ fi
 # install oh-my-zsh
 if ! [ -e ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL \
-    https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
 fi
 
 if [ $1 == "remote" ]
 then
-    cp ./.remote.zshrc ~/.zshrc
+    cp ./.remote.zshrc ~/.zshrc &&
 else
-    cp ./.local.zshrc ~/.zshrc
+    cp ./.local.zshrc ~/.zshrc &&
 fi
 cp ./.psqlrc ~ &&
 cp -r ./.ssh ~/.ssh
