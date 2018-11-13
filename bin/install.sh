aed87f9c03cbe0bcc7b6be84d14ea5cf9b474840
Brennon Schow <bschow@crossroadstech.net>
2018-11-13 14:03:14 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-# ensure curl is installed
+# setup -- extended glob support and curl
+shopt -s extglob
+
 if ! [ -x "$(command -v curl)" ]; then
     sudo apt install curl -y
 fi
 
+# identify server type
+exclude='local'
+if [ $1 == 'local' ]; then
+    exclude='remote'
+fi
+
 # copy necessary files to home,
 # install oh-my-zsh
-cp -r ./.[^git.]* ~ &&
+cp -r ./.[^(git|.|$exclude)]* ~ &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
