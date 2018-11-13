0b2586b4f0eac542a45243ac340e0e549d2396fa
Brennon Schow <bschow@crossroadstech.net>
2018-11-13 15:06:36 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -13,6 +13,6 @@ then
 else
     cp ./.local.zshrc ~/.zshrc
 fi
-cp ./.psql ~ &&
+cp ./.psqlrc ~ &&
 cp -r ./.ssh ~/.ssh &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
