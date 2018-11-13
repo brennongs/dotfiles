fc9dad625cc774a1e3cd8bfab52ec95474f6ad55
Brennon Schow <bschow@crossroadstech.net>
2018-11-13 14:21:50 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -14,6 +14,6 @@ fi
 # copy necessary files to home,
 # install oh-my-zsh
 sh -c "$(curl -fsSL \
-https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
 cp -r ./.^(git|*.zshrc|.)* ~ &&
 cp ./.^($exclude).zshrc ~/.zshrc &&
