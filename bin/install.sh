e8c92fa4b20f2a2e0ebf31d889ef483f87cd17d4
Brennon Schow <bschow@crossroadstech.net>
2018-11-14 08:23:49 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 # ensure curl, zsh
-if ! [ -x "$(command -v curl)" ]; then
+if ! [[ -x "$(command -v curl)" ]]; then
     sudo apt install curl zsh -y
 fi
 
-if ! [ -e '~/.oh-my-zsh' ]
-then
+if ! [[ -d ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL \
     https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
