b1705cfe2090ba060b5078da979b088f9652c88b
Brennon Schow <bschow@crossroadstech.net>
2018-11-14 08:05:56 -0700

    updating dotfiles from LCID001

--- bin/update.sh
+++ bin/update.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# copy necessary files to this dir
-cp ~/.zshrc .
-printf "\nCopied .zshrc!\n"
-cp ~/.psqlrc .
-printf "\nCopied .psqlrc!\n\n"
-
-# push to git
 git add . ./.[a-z]*
 git commit -am "updating dotfiles from $HOSTNAME"
 git push
