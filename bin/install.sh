05e714aa852dd90baa263711495b00b76c1a063f
Brennon Schow <bschow@crossroadstech.net>
2018-11-14 08:34:46 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -7,7 +7,7 @@ fi
 
 if ! [[ -d ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL \
-    https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 fi
 
