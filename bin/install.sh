8f2b480cb9e5b570b4834318950891d23be3646b
Brennon Schow <bschow@crossroadstech.net>
2018-11-20 14:05:04 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # ensure curl, zsh
-if ! [[ -x "$(command -v curl)" ]]; then
-    sudo apt install curl zsh -y
-fi
+sudo apt install curl zsh -y
+chsh -s $(which zsh) $USER
 
-if ! [[ -d ~/.oh-my-zsh ]]; then
-    sh -c "$(curl -fsSL \
-    https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k &&
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
+sh \
+    -c "$(curl \
+        -fsSLi https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
+git clone \
+    https://github.com/bhilburn/powerlevel9k.git \
+    ~/.oh-my-zsh/custom/themes/powerlevel9k &&
+git clone \
+    https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # copy necessary files to ~
 cp ./.zshrc ~
