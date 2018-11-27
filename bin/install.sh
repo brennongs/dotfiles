f44b8f794ac9f4ba342d4a1831007e6fa5eb3229
Brennon Schow <bschow@crossroadstech.net>
2018-11-27 11:23:33 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -4,15 +4,21 @@
 sudo apt install curl zsh -y
 chsh -s $(which zsh) $USER
 
-sh \
-    -c "$(curl \
-        -fsSLi https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone \
-    https://github.com/bhilburn/powerlevel9k.git \
-    ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone \
-    https://github.com/zsh-users/zsh-syntax-highlighting.git \
-    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+function install {
+    sh \
+        -c "$(curl \
+            -fsSLi https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone \
+        https://github.com/bhilburn/powerlevel9k.git \
+        ~/.oh-my-zsh/custom/themes/powerlevel9k
+    git clone \
+        https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+}
+
+if [[ -d ~/.oh-my-zsh ]]; then
+    install
+fi
 
 # copy necessary files to ~
 cp ./.zshrc ~
