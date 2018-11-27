fc2d56d5416aedccf722f8695936708732290c45
Brennon Schow <bschow@crossroadstech.net>
2018-11-27 11:20:09 -0700

    updating dotfiles from LCID001

--- bin/install.sh
+++ bin/install.sh
@@ -6,10 +6,10 @@ chsh -s $(which zsh) $USER
 
 sh \
     -c "$(curl \
-        -fsSLi https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
+        -fsSLi https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone \
     https://github.com/bhilburn/powerlevel9k.git \
-    ~/.oh-my-zsh/custom/themes/powerlevel9k &&
+    ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone \
     https://github.com/zsh-users/zsh-syntax-highlighting.git \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
